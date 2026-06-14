1c5173b89c98fe17167e50981575692fb746542b
btlaube <69732464+btlaube@users.noreply.github.com>
2026-06-14 15:52:39 -0500

    likely final changes

--- Metroidvania32/Assets/Scripts/HazardCollider.cs
+++ Metroidvania32/Assets/Scripts/HazardCollider.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class HazardCollider : MonoBehaviour
 {
-    [SerializeField] private float damageAmount = 9999f; // Amount of damage to deal to the player
+    [SerializeField] private float damageAmount = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("PlayerHitbox"))
         {
-            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            PlayerHealth playerHealth = other.transform.parent.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
-                playerHealth.TakeDamage(damageAmount); // Instantly kill the player
+                playerHealth.TakeDamage(damageAmount);
             }
         }
     }
